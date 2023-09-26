eb9b9165d1239c624d56da78ec746f0205772047
unknown <A90455@itri.org.tw>
2023-09-26 17:42:19 +0800

    車輛移動&障礙物完成已可以遊玩

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,13 @@
 #include <QTimer>
 #include <QDebug>
 #include <QKeyEvent>
-#define BLOCK01_X_POS 300
+#define CLOCK_TIMEINTERVAL 1000		// 時間計時器間隔
+#define OBJECT_TIMEINTERVAL 10		// 物體計時器間隔
+#define GAME_TIME 6			        // 遊戲時間
+#define CAR_SPEED 5			        // 車子移動速度(水平)
+#define CAR_MOVE_SPEED 2		    // 車子移動速度(垂直)
+#define CAR_INIT_Y_POS 220		    // 車子初始位置(垂直)
+#define BLOCK01_X_POS 1200
 #define BLOCK01_Y_POS 40
 #define BLOCK02_X_POS 2300
 #define BLOCK02_Y_POS 110
@@ -43,13 +49,13 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     bgm_pos = 0;
     object_timer = new QTimer(this);  //建立計時器
     connect(object_timer, SIGNAL(timeout()), this, SLOT(update_object())); //連接訊號
-    object_timer->start(10);  //每10毫秒更新一次
+    object_timer->start(OBJECT_TIMEINTERVAL);  //每10毫秒更新一次
 
     //移動時間
     time = 30;
     clock_timer = new QTimer(this);  //建立計時器
     connect(clock_timer, SIGNAL(timeout()), this, SLOT(update_time())); //連接訊號
-    //clock_timer->start(1000); //每1秒倒數一次
+    //clock_timer->start(CLOCK_TIMEINTERVAL); //每1秒倒數一次
 
 }
 
@@ -60,18 +66,18 @@ MainWindow::~MainWindow(){
 void MainWindow::update_object(){
    // qDebug() << "timeup!!!!!!";
 
-    bgm_pos -=1;   //背景位置往左移動1
+    bgm_pos -=1 * CAR_SPEED;   //背景位置往左移動1
 
     ui->background->setGeometry(QRect(bgm_pos, 0, 4800, 512 )); //設定背景位置
 
-    if(bgm_pos == -32){
+    if(bgm_pos == -32 * CAR_SPEED){
         //移動一圈，背景從頭顯示
         bgm_pos = 0;
     }
 
     if(game_status==1){
         //playing
-        car_distance +=1;
+        car_distance +=1 * CAR_SPEED;
         ui->label_distance->setText("距離: " + QString::number(car_distance,'f',2)+"m");//顯示移動距離
         move_car();      // 呼叫 move_car()移動車子
         move_blocks();   // 呼叫 move_blocks()移動障礙物
@@ -115,17 +121,17 @@ void MainWindow::game_start(){
     ui->label_title->setVisible(false);       //隱藏遊戲標題
     ui->button_start_game->setVisible(false); //隱藏開始遊戲按鈕
 
-    clock_timer->start(1000);                 //每隔一秒更新一次
-    time = 30;                                //重設遊戲時間
+    clock_timer->start(CLOCK_TIMEINTERVAL);   //每隔一秒更新一次
+    time = GAME_TIME;                         //重設遊戲時間
     ui->lcd_clock->display(time);             //顯示遊戲時間
 
-    car_pos = 220;                            //重設車子的垂直位置(位於中央)
+    car_pos = CAR_INIT_Y_POS;                 //重設車子的垂直位置(位於中央)
     car_distance =  0;                        //重設移動距離
     car_direction = 0;                        //設定初始方向
 
     if(!object_timer->isActive()){
         // 若物體計時器未啟用，則讓他啟用 (遊戲結束會停止)
-        object_timer->start(10);
+        object_timer->start(OBJECT_TIMEINTERVAL);
     }
     ui->actionstart_game->setText("暫停遊戲"); //設定選單[開始遊戲]按鈕的文字
 }
@@ -141,11 +147,15 @@ void MainWindow:: keyPressEvent(QKeyEvent *event){
             car_direction = 1;
             qDebug()<< "down";
             break;
+        case Qt::Key_Space:
+            game_pause();
+            qDebug()<< "space";
+            break;
 
     }
 }
 void MainWindow::move_car(){
-    int car_new_pos = car_pos + car_direction;  //接下來要移動的位置站存起來
+    int car_new_pos = car_pos + car_direction *CAR_MOVE_SPEED;  //接下來要移動的位置站存起來
 
     if((car_new_pos)>=35 && (car_new_pos)<=410){ //判斷汽車是否超出邊界
         //沒有超過跑到邊緣
@@ -251,8 +261,8 @@ void MainWindow::game_pause(){
         // pause 變為playing
         game_status = 1;  //設為playing的狀態
 
-        clock_timer->start(1000); // 開始時間計時器
-        object_timer->start(10); // 開始物體移動計時器
+        clock_timer->start(CLOCK_TIMEINTERVAL); // 開始時間計時器
+        object_timer->start(OBJECT_TIMEINTERVAL); // 開始物體移動計時器
 
         ui->label_title->setVisible(false);  //顯示標題圖片
         ui->actionstart_game->setText("暫停遊戲"); //設定選單[暫停遊戲]按鈕的文字
